7522f4166f092fcc8a0143bc03d3f956eb2684fa
Moyinks <moyinksv@gmail.com>
2026-06-11 22:42:35 +0100

    Update 2026-06-11 22:42

--- sw.js
+++ sw.js
@@ -31,7 +31,7 @@
  *      Ensures the app opens offline after first visit.
  */
 
-var CACHE_NAME    = 'qs-v1';
+var CACHE_NAME    = 'qs-v1.75';
 var IMAGE_CACHE   = 'qs-images-v4.0';
 var MARKET_CACHE  = 'qs-market-v1.0';
 /* ── Install ─────────────────────────────────────────────────────────────────
